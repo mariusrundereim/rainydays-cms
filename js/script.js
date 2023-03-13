673b691fe6ea4c252d366140f91af10ba38c20f2
mariusrundereim <marius.rundereim@gmail.com>
2023-03-13 15:58:41 +0100

    fetch url

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-const url = "https://localhost/rainydays-cms/wp-json/wc/store/products";
+const url = `https://runder.no/rainydays-cms/wp-json/wc/store/products/`;
 
 async function getProducts() {
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
-}
 
+  data.forEach((jacket) => {
+    console.log(jacket.name);
+    console.log(jacket.id);
+    console.log(jacket.images[0].src);
+  });
+}
 getProducts();
+
+/* Old functions */
 
 function men() {
   document.querySelector(".category-men").classList.toggle("display-block");
