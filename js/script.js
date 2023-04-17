23fe66217d268ee6f1d7b628dfd4f2b16cee50e1
mariusrundereim <marius.rundereim@gmail.com>
2023-04-17 15:25:12 +0200

    changes in navigation on all pages

--- js/script.js
+++ js/script.js
@@ -23,18 +23,3 @@ async function getProducts() {
 }
 getProducts();
 
-/* Old functions */
-
-function men() {
-  document.querySelector(".category-men").classList.toggle("display-block");
-}
-
-function women() {
-  document.querySelector(".category-women").classList.toggle("display-block");
-}
-
-function MobileBar() {
-  document
-    .querySelector(".header-mobile-pagelinks")
-    .classList.toggle("display-block");
-}
